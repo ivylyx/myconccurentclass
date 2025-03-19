2402217698d44b05caf2d1bd9b6606d93abd93b2
ivy_lyx <183441693@qq.com>
2025-03-19 13:09:25 +0800

    test commit

---
+++ MyThreadPool.java
@@ -0,0 +1,17 @@
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class MyThreadPool {
+
+    private int corePoolSize;
+    private int maxPoolSize;
+    private BlockingQueue<Runnable> taskList;
+
+    public MyThreadPool(int corePoolSize,int maxPoolSize){
+        ThreadPoolExecutor
+    }
+    void execute(Runnable command){
+
+    }
+}
---
+++ RejectHandler.java
@@ -0,0 +1,3 @@
+public interface RejectHandler {
+    void handle();
+}
